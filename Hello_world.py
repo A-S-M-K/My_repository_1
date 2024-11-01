f74303525c8949141648b72fa44dae18b6d3ce0f
A-S-M-R <atharvasm23@gmail.com>
2024-11-01 21:10:53 +0530

    Changes on Git

--- Hello_world.py
+++ Hello_world.py
@@ -1,2 +1,6 @@
 X = input("Enter Your Name: ")
-print ("Hello"+X)
+print ("Hello "+X)
+if 'a' in X or 'e' in X or 'i' in X or 'u' in X:
+    print("There is at least one vowel pesent in your name "+ X)
+else:
+        print("There is no vowel in your name "+ X)
